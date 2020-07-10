efe3f872af626963de6d6d2f204a308cc8e666e7
Fred Emmott <fred@fredemmott.com>
2020-07-09 17:30:11 -0700

    Implement feedback sounds on MacOS

--- Sources/PlayWavFileMacOS.cpp
+++ Sources/PlayWavFileMacOS.cpp
@@ -1,3 +1,25 @@
 #include "PlayWavFile.h"
 
-void PlayWavFile(const std::string& _path) { /* TODO */ }
+#include <AudioToolbox/AudioToolbox.h>
+#include <CoreFoundation/CoreFoundation.h>
+
+#include <map>
+
+void PlayWavFile(const std::string& path) {
+  static std::map<std::string, SystemSoundID> ids;
+  const auto it = ids.find(path);
+  if (it == ids.end()) {
+    CFStringRef cfpath
+      = CFStringCreateWithCString(NULL, path.c_str(), kCFStringEncodingUTF8);
+    CFURLRef cfurl
+      = CFURLCreateWithFileSystemPath(NULL, cfpath, kCFURLPOSIXPathStyle, false);
+    SystemSoundID id;
+    AudioServicesCreateSystemSoundID(cfurl, &id);
+    ids.emplace(path, id);
+
+    CFRelease(cfurl);
+    CFRelease(cfpath);
+  }
+  const auto id = ids.at(path);
+  AudioServicesPlaySystemSound(id);
+}
