986e87896acb9117775fea1f66e7ffb2952ccb76
Fred Emmott <fred@fredemmott.com>
2021-02-12 13:47:06 -0600

    attempt to build a fat binary for macos in CI

--- StreamDeckSDK.cmake
+++ StreamDeckSDK.cmake
@@ -2,11 +2,12 @@ include(ExternalProject)
 
 ExternalProject_Add(
   StreamDeckSDK_build
-  URL https://github.com/fredemmott/StreamDeck-CPPSDK/releases/download/v2.0-rc6/StreamDeckSDK-v2.0-rc6.zip
-  URL_HASH SHA512=a5dfa7b30dffeac0bda75b7da929794cd08b48eb63530894952b6edbd1e7999abb52d65ad267eb3e8931d29d8a2688dc13b8032d04d1d9f9c46e6fcf12946954
+  URL https://github.com/fredemmott/StreamDeck-CPPSDK/releases/download/v2.0-rc7/StreamDeckSDK-v2.0-rc7.zip
+  URL_HASH SHA512=38909e627b7a1284cb4c22f80b999d8dcb55e53719cb061fe7d711c1426e69405d5257c07bae02e1417f58d1a03df6f23b557dd6f27b55fd24faea8779f1d604
   CMAKE_ARGS
     -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
     -DCMAKE_MSVC_RUNTIME_LIBRARY=${CMAKE_MSVC_RUNTIME_LIBRARY}
+    -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES}
     -DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
 )
 
