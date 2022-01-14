ffce0c2570db6361bb782cec4cc74957fad630c1
Fred Emmott <git@fredemmott.com>
2022-01-14 09:07:20 -0600

    Update to latest streamdeck SDK
    Makes `make install` work :)

--- StreamDeckSDK.cmake
+++ StreamDeckSDK.cmake
@@ -2,8 +2,8 @@ include(ExternalProject)
 
 ExternalProject_Add(
   StreamDeckSDK_build
-  URL https://github.com/fredemmott/StreamDeck-CPPSDK/releases/download/v2.0-rc13/StreamDeckSDK-v2.0-rc13.zip
-  URL_HASH SHA512=db2e22c82e7b70006d21141ef1833d9c60d62303f1b165e7b5c6e3b86b30bc8cb1e9edbd67a0e82adf2823336eb0e3d131bb43f6abbbed8183aa15c199bd3034
+  URL https://github.com/fredemmott/StreamDeck-CPPSDK/releases/download/v2.0.2/StreamDeckSDK-v2.0.2.zip
+  URL_HASH SHA512=df735047900947ac3ea0e9137368a5b270edf7d51156bd1a48ba4d753509a44a7e68bd1ea09e36a2dee95adb002210624e0f28ce833e7d7c4b9895e588e43c4f
   CMAKE_ARGS
     -DBUILD_LIB_ONLY=ON
     -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
@@ -43,8 +43,20 @@ elseif(WIN32)
 elseif(UNIX AND NOT APPLE)
   target_link_libraries(StreamDeckSDK INTERFACE pthread)
 endif()
+
 set(
   STREAMDECK_PLUGIN_DIR
   ${STREAMDECK_PLUGIN_DIR}
   CACHE PATH "Path to this system's streamdeck plugin directory"
 )
+
+function(set_default_install_dir_to_streamdeck_plugin_dir)
+  if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
+    set(
+      CMAKE_INSTALL_PREFIX
+      "${STREAMDECK_PLUGIN_DIR}/${CMAKE_PROJECT_NAME}"
+      CACHE PATH "See cmake documentation"
+      FORCE
+    )
+  endif()
+endfunction()
